b54485eafc4fe29e7a3f4037a18693fbc623fe15
Scottland Donaldson <scott.p.donaldson@gmail.com>
2015-11-11 16:18:04 -0500

    IT RENDERS... AGAIN

--- js/src/components/MainComponent.js
+++ js/src/components/MainComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TWEEN from 'tween.js';
+import THREE from 'three.js';
 
 class Main extends React.Component {
 	
@@ -23,20 +23,58 @@ class Main extends React.Component {
 
 		let canvas = ReactDOM.findDOMNode(this);
 
-		canvas.width = canvas.parentNode.clientWidth;
-		canvas.height = canvas.parentNode.clientHeight;
-
-		window.addEventListener('resize', () => {
-			// TODO: update canvas on resize
+		let Scene = new THREE.Scene();
+		let Renderer = new THREE.WebGLRenderer({
+			antialias: true,
+	        preserveDrawingBuffer: true,
+			canvas
 		});
+		Renderer.setClearColor('#555');
+		Renderer.setSize((window.devicePixelRatio || 1) * canvas.parentNode.clientWidth, (window.devicePixelRatio || 1) * canvas.parentNode.clientHeight);
 
-		this._paint(canvas);
+		let BoxGeometry = new THREE.BoxGeometry(20, 20, 20);
+		let BoxMaterial = new THREE.MeshLambertMaterial({
+			color: '#fff'
+		});
+		let BoxMesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
+		Scene.add(BoxMesh);
 
-		this.props.auth.on('login', () => {
-			this.setState({
-				fill: '#fe0'
-			});
-			this._paint(canvas);
+		let Camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 10000);
+		Scene.add(Camera);
+
+		let Light = new THREE.AmbientLight('#fff');
+		Scene.add(Light);
+
+		Camera.position.set(120, 120, 120);
+		Camera.lookAt(BoxMesh.position);
+
+		canvas.style.width = '100%';
+		canvas.style.height = '100%';
+
+		let t = 0;
+
+		let _render = () => {
+			
+			Renderer.render(Scene, Camera);
+
+			BoxMesh.position.setX( 100 * Math.sin(t / 100) );
+
+			t++;
+			
+			window.requestAnimationFrame(_render);
+		};
+
+		_render();
+
+		window.addEventListener('resize', function() {
+
+			Camera.aspect = canvas.parentNode.clientWidth / canvas.parentNode.clientHeight;
+			Camera.updateProjectionMatrix();
+
+			Renderer.setSize((window.devicePixelRatio || 1) * canvas.parentNode.clientWidth, (window.devicePixelRatio || 1) * canvas.parentNode.clientHeight);
+
+			canvas.style.width = '100%';
+			canvas.style.height = '100%';
 		});
 	}
 
